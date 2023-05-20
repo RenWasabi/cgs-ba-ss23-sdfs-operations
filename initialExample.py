dd4d31afa5942d229a860a8ea89c98a521907fff
Caro <renwasabi@t-online.de>
2023-05-20 14:27:20 +0200

    set up curve network, almost mesh for displaying values

--- initialExample.py
+++ initialExample.py
@@ -2,6 +2,7 @@ import polyscope as ps
 import numpy as np
 import functions
 import marching_squares as ms
+import sys # for printing max
 
 
 # todo
@@ -15,6 +16,7 @@ import marching_squares as ms
 # - currently I'm just using any implicit functions -> use signed distance functions
 #
 # - visualize SDF (function/distance) values
+# - make actual mesh
 # - use vectors for x y coordinates
 #
 # project isonlines to bottom plane instead of current one
@@ -83,16 +85,120 @@ squares_circles_subtract = ms.MS_Grid(vertice_grid, 0, functions.subtract(unit_c
 circles_subtract_points, circles_subtract_edges = squares_circles_subtract.full_run()
 shape_substract = ps.register_curve_network("circles_subtract", circles_subtract_points, circles_subtract_edges)
 
-
-# visualizing functions values on z axis
+"""
+# visualizing functions values on z axis, as vectors
 #value_vectors = np.ndarray((vertice_grid.shape[0]*vertice_grid.shape[1], 3))
 value_vectors_points = []
 value_vectors_edges = []
 value_vectors_edge_colors = []
 point_index_counter = 0
 edge_index_counter = 0
-for i in range(0,vertice_grid_shape[0],10):
-    for j in range(0,vertice_grid_shape[1], 10):
+for i in range(0,vertice_grid_shape[0],25):
+    for j in range(0,vertice_grid_shape[1], 25):
+        # vertice_grid[i,j,2] = 100 should have the correct value still from ms squares
+        base = np.asarray([vertice_grid[i,j,0], vertice_grid[i,j,1], 0]) # the coordinate
+        top = np.asarray([vertice_grid[i,j,0], vertice_grid[i,j,1], vertice_grid[i,j,2]])
+        value_vectors_points.append(base)
+        value_vectors_points.append(top)
+        value_vectors_edges.append(np.asarray([point_index_counter, point_index_counter+1]))
+
+        # value <= 0 blue, else red
+        if (vertice_grid[i,j,2] <= 0): 
+            value_vectors_edge_colors.append(np.asarray([0,0,1]))
+        else:
+            value_vectors_edge_colors.append(np.asarray([1,0,0]))
+        point_index_counter += 2
+value_vectors_points = np.asarray(value_vectors_points)
+value_vectors_edges = np.asarray(value_vectors_edges)
+value_vectors_edge_colors = np.asarray(value_vectors_edge_colors)
+value_net = ps.register_curve_network("my_value_vectors", value_vectors_points, value_vectors_edges, transparency=0.3)
+value_net.add_color_quantity("my_value_colors", value_vectors_edge_colors, defined_on="edges", enabled=True)
+"""
+
+# visualizing functions values on z axis
+#value_vectors = np.ndarray((vertice_grid.shape[0]*vertice_grid.shape[1], 3))
+resolution_step = 25
+mesh_grid_x = np.int64(np.ceil(vertice_grid.shape[0]/resolution_step))
+mesh_grid_y = np.int64(np.ceil(vertice_grid.shape[1]/resolution_step))
+
+number_mesh_points = mesh_grid_x*mesh_grid_y
+
+number_x_edges = mesh_grid_x*(mesh_grid_y-1)
+number_y_edges = (mesh_grid_x-1)*mesh_grid_y
+number_diag_edges = (mesh_grid_x-1)*(mesh_grid_y-1)
+number_total_edges = number_x_edges + number_y_edges + number_diag_edges
+
+value_mesh_points = np.ndarray((mesh_grid_x*mesh_grid_y, 3)) # flat array of 3D mesh points
+
+value_mesh_edges = np.ndarray((number_total_edges, 2)) # flat array of edges (as length 2 array)
+
+for i in range(mesh_grid_x): 
+    for j in range(mesh_grid_y):
+        # the points
+        value_mesh_points[i*mesh_grid_y+j,:] = vertice_grid[i*resolution_step, j*resolution_step, :]
+
+        # the edges
+        # x direction edges 
+        if (j < mesh_grid_y-1):
+            value_mesh_edges[i*(mesh_grid_y-1)+j, :] = np.asarray([i*mesh_grid_y+j, i*mesh_grid_y+j+1])
+
+        # the y direction edges
+        if (i < mesh_grid_x and (j != 0 or i==0) and i*(mesh_grid_y-1)+j+number_x_edges != number_x_edges+number_y_edges):
+            value_mesh_edges[i*(mesh_grid_y-1)+j+number_x_edges, :] = np.asarray([i*(mesh_grid_y-1)+j, (i+1)*mesh_grid_y+j-i])
+
+
+
+
+        # the diagonal edges
+        if (i >= mesh_grid_x-1 or j >= mesh_grid_y-1):
+            continue
+        else:
+            print("i:", i, "j:", j)
+            print("value_mesh_edges[",i*(mesh_grid_y-1)+j+number_x_edges+number_y_edges, "] = ",i*mesh_grid_y+j, (i+1)*mesh_grid_y+j+1) 
+            value_mesh_edges[i*(mesh_grid_y-1)+j+number_x_edges+number_y_edges,:] = np.asarray([i*mesh_grid_y+j, (i+1)*mesh_grid_y+j+1])
+                
+
+ps.register_curve_network("my_future_mesh", value_mesh_points, value_mesh_edges)  
+
+#np.set_printoptions(threshold=sys.maxsize)
+print(value_mesh_edges)
+print("total points: ", number_mesh_points)
+print("total edges", number_total_edges)
+print("x, ", number_x_edges)
+print("y", number_y_edges)
+print("diag" , number_diag_edges)
+print(value_mesh_edges[0])
+print(value_mesh_edges[1])
+print(value_mesh_edges[number_x_edges-1])
+print("oben: x, unten: y")
+print(value_mesh_edges[number_x_edges])
+print(value_mesh_edges[number_x_edges+number_y_edges-15])
+print("ab hier")
+print(value_mesh_edges[number_x_edges+number_y_edges-14])
+print(value_mesh_edges[number_x_edges+number_y_edges-1])
+print("oben: y, unten: diag")
+print(value_mesh_edges[number_x_edges+number_y_edges])
+print(value_mesh_edges[number_x_edges+number_y_edges+1])
+print(value_mesh_edges[number_x_edges+number_y_edges+2])
+print(value_mesh_edges[number_x_edges+number_y_edges+14])
+print(value_mesh_edges[number_x_edges+number_y_edges+15])
+print(value_mesh_edges[number_total_edges-15])
+print(value_mesh_edges[number_total_edges-16])
+print(value_mesh_edges[number_total_edges-17]) 
+print(value_mesh_edges[number_total_edges-20])
+print("bis hier ok")
+print(value_mesh_edges[number_total_edges-1])
+print(value_mesh_edges[number_total_edges-2])
+print(value_mesh_edges[number_total_edges-3])
+print(value_mesh_edges[number_total_edges-10])
+print(value_mesh_edges[number_total_edges-14])
+print("mesh x", mesh_grid_x)
+print("mesh_y", mesh_grid_y)
+
+
+"""
+for i in range(0,vertice_grid_shape[0],25):
+    for j in range(0,vertice_grid_shape[1], 25):
         # vertice_grid[i,j,2] = 100 should have the correct value still from ms squares
         base = np.asarray([vertice_grid[i,j,0], vertice_grid[i,j,1], 0]) # the coordinate
         top = np.asarray([vertice_grid[i,j,0], vertice_grid[i,j,1], vertice_grid[i,j,2]])
@@ -112,6 +218,6 @@ value_vectors_edge_colors = np.asarray(value_vectors_edge_colors)
 value_net = ps.register_curve_network("my_value_vectors", value_vectors_points, value_vectors_edges, transparency=0.3)
 value_net.add_color_quantity("my_value_colors", value_vectors_edge_colors, defined_on="edges", enabled=True)
         
-        
+"""
 
 ps.show()
