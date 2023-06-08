12219c5d96b1403dbc5006bcb1e9076c3b38bdf1
Caro <renwasabi@t-online.de>
2023-06-08 15:27:36 +0200

    added ui selection for boolean operation

--- main.py
+++ main.py
@@ -56,13 +56,18 @@ helper.hlp.ps_register_and_list_whole_FOB(FOBfunction1c, name1c, example1_visual
 
 
 #  EXAMPLE BOOLEAN OPERATIONS START
-example2_visuals = []
+example2_visuals = [] # all visuals belonging to this example
+example2_union = [] # all visuals belonging to union
+example2_intersection = []
+example2_complement = []
+example2_square_subtract_circle = []
+example2_circle_subtract_square = []
 
 center_x2 = 0
 center_y2 = 0
 sidelength2 = 4
-resolution2 = 0.05
-resolution_step2 = 15
+resolution2 = 0.01
+resolution_step2 = 5
 isovalue2 = 0
 
 square1 = functions.rectangle_function(center_x2-0.3, center_y2+0, 1, 1)
@@ -76,21 +81,46 @@ function2difference_circle = functions.subtract(circle1, square1)
 
 # render all in same spot, choose enabled one with button
 
+# union
 FOBfunction2union = fob.FOB(function2union, isovalue2, center_x2, center_y2, sidelength2, resolution2, resolution_step2)
 name2union = "function2union"
-helper.hlp.ps_register_and_list_whole_FOB(FOBfunction2union, name2union, example2_visuals)
+helper.hlp.ps_register_and_list_whole_FOB(FOBfunction2union, name2union, example2_visuals) 
+# the last 3 structures belong to union
+example2_union.append(example2_visuals[len(example2_visuals)-3])
+example2_union.append(example2_visuals[len(example2_visuals)-2])
+example2_union.append(example2_visuals[len(example2_visuals)-1])
+
+# intersection
 FOBfunction2intersection = fob.FOB(function2intersection, isovalue2, center_x2, center_y2, sidelength2, resolution2, resolution_step2)
 name2intersection = "function2intersection"
 helper.hlp.ps_register_and_list_whole_FOB(FOBfunction2intersection, name2intersection, example2_visuals)
+example2_intersection.append(example2_visuals[len(example2_visuals)-3])
+example2_intersection.append(example2_visuals[len(example2_visuals)-2])
+example2_intersection.append(example2_visuals[len(example2_visuals)-1])
+
+# complement
 FOBfunction2complement = fob.FOB(function2complement, isovalue2, center_x2, center_y2, sidelength2, resolution2, resolution_step2)
 name2complement = "function2complement"
 helper.hlp.ps_register_and_list_whole_FOB(FOBfunction2complement, name2complement, example2_visuals)
+example2_complement.append(example2_visuals[len(example2_visuals)-3])
+example2_complement.append(example2_visuals[len(example2_visuals)-2])
+example2_complement.append(example2_visuals[len(example2_visuals)-1])
+
+# subtract circle from square
 FOBfunction2difference_square = fob.FOB(function2difference_square, isovalue2, center_x2, center_y2, sidelength2, resolution2, resolution_step2)
 name2difference_square = "function2difference_square"
 helper.hlp.ps_register_and_list_whole_FOB(FOBfunction2difference_square, name2difference_square, example2_visuals)
+example2_square_subtract_circle.append(example2_visuals[len(example2_visuals)-3])
+example2_square_subtract_circle.append(example2_visuals[len(example2_visuals)-2])
+example2_square_subtract_circle.append(example2_visuals[len(example2_visuals)-1])
+
+# subtract square from circle
 FOBfunction2difference_circle = fob.FOB(function2difference_circle, isovalue2, center_x2, center_y2, sidelength2, resolution2, resolution_step2)
 name2difference_circle = "function2difference_circle"
 helper.hlp.ps_register_and_list_whole_FOB(FOBfunction2difference_circle, name2difference_circle, example2_visuals)
+example2_circle_subtract_square.append(example2_visuals[len(example2_visuals)-3])
+example2_circle_subtract_square.append(example2_visuals[len(example2_visuals)-2])
+example2_circle_subtract_square.append(example2_visuals[len(example2_visuals)-1])
 
 
 
@@ -137,9 +167,25 @@ ui_text = "some input text"
 ui_example_options = [ "None", "Level Set Methods", "Boolean Operations"]
 ui_example_options_selected = ui_example_options[0]
 
+# initially, the visibiliy of all structures is disabled
 for example in ui_example_options:
     for structure in example_dict[example]:
         structure.set_enabled(False)
+
+# options for boolean operations example
+ui_boolean_operations = ["None","Union", "Intersection", "Complement", "Square\Circle", "Circle\Square" ]
+ui_boolean_operation_selected = ui_boolean_operations[0]
+boolean_op_dict = {
+    "None" : [],
+    "Union": example2_union,
+    "Intersection" : example2_intersection,
+    "Complement" : example2_complement,
+    "Square\Circle" : example2_square_subtract_circle,
+    "Circle\Square" : example2_circle_subtract_square
+}
+
+
+
 
 
 def my_function():
@@ -152,7 +198,7 @@ def my_function():
 def callback():
 
 
-    global ui_text, ui_example_options, ui_example_options_selected, active_example
+    global ui_text, ui_example_options, ui_example_options_selected, active_example, ui_boolean_operations, ui_boolean_operation_selected
 
 
 
@@ -188,13 +234,36 @@ def callback():
                     # change visibility
                     for structure in example_dict[active_example]:
                         structure.set_enabled(False)
-                    for structure in example_dict[example]:
-                        structure.set_enabled(True)
+                    # for Boolean operations we still want to select the specific operation to be displayed
+                    if example != "Boolean Operations":
+                        for structure in example_dict[example]:
+                            structure.set_enabled(True)
                     active_example = example
                 ui_example_options_selected = example
         psim.EndCombo()
     psim.PopItemWidth()
 
+
+    # Boolean Operations
+    if (active_example == ui_example_options[2]):
+
+        # Choose the Boolean operation
+        psim.PushItemWidth(200)
+        changed = psim.BeginCombo("Choose operation", ui_boolean_operation_selected)
+        if changed:
+            for operation in ui_boolean_operations:
+                _, selected = psim.Selectable(operation, ui_boolean_operation_selected==operation)
+                if selected:
+                    # everything in this if block is only executed once when the example is changed
+                    if ui_boolean_operation_selected != operation:
+                        print("Changed boolean Operation")
+                        for structure in boolean_op_dict[ui_boolean_operation_selected]:
+                            structure.set_enabled(False)
+                        for structure in boolean_op_dict[operation]:
+                            structure.set_enabled(True)
+                    ui_boolean_operation_selected = operation
+            psim.EndCombo()
+        psim.PopItemWidth()
 
 
 
